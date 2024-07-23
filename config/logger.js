af2221be2c302930eb4335b69b725e99027c6cd4
Lakshmanan <lakshmanan@ariveguru.com>
2024-07-23 16:04:20 +0530

    2nd commit logger attach

---
+++ config/logger.js
@@ -0,0 +1,19 @@
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+logger.add(new transports.File({ filename: "error.log", level: "error" }));
+
+module.exports = logger;
--- controller/company.controller.js
+++ controller/company.controller.js
@@ -1,29 +1,50 @@
 const { companyModel } = require("../models/company.model");
 const { postCompanySchema } = require("../validations/company.validation");
+const logger = require("../config/logger");
 
 exports.postCompany = async (req, res) => {
-  console.log(req.body, "req.body");
-  const { error, value } = postCompanySchema.validate(req.body);
-  const newCompany = await companyModel.create(value);
-  if (error) {
-    return res.status(400).json({
-      msg: error.details[0].message,
+  try {
+    logger.info(JSON.stringify(req.body));
+    const { error, value } = postCompanySchema.validate(req.body);
+    const newCompany = await companyModel.create(value);
+    if (error) {
+      logger.error(error);
+      return res.status(400).json({
+        msg: error.details[0].message,
+      });
+    }
+
+    return res.status(201).json({
+      msg: "COMPANY CREATED SUCCESSFULLY",
+      data: newCompany,
+    });
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).json({
+      msg: "Internal server error",
     });
   }
-
-  return res.status(201).json({
-    msg: "COMPANY CREATED SUCCESSFULLY",
-    data: newCompany,
-  });
 };
 
 exports.getCompany = async (req, res) => {
   try {
+    logger.info(JSON.stringify(req.body));
+
     const { page, pageSize, search } = req.body;
     const skip = (page - 1) * pageSize;
-
+    const match = search
+      ? {
+          $or: [
+            { comapanyName: { $regex: search, $options: "i" } },
+            { country: { $regex: search, $options: "i" } },
+            {
+              "productPortFolio.productName": { $regex: search, $options: "i" },
+            },
+          ],
+        }
+      : {};
     const comapnies = await companyModel
-      .find({})
+      .find(match)
       .skip(skip)
       .limit(pageSize)
       .lean();
@@ -34,6 +55,7 @@ exports.getCompany = async (req, res) => {
     });
   } catch (e) {
     console.log(e);
+    logger.error(e);
     return res.status(500).json({
       msg: "Internal server error",
     });
